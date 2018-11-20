8064d900afaea6f84ce602e35724d7b870064fba
Chris <chris.tramier97@gmail.com>
2018-11-20 15:10:05 +0100

    systemjs.config.js edit to use Unpkg

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -1,42 +1,41 @@
 /**
- * System configuration for Angular samples
- * Adjust as necessary for your application needs.
+ * Version de SystemJS dédié à la production.
+ * Les paquets sont chargés depuis le web.
  */
 (function (global) {
-    System.config({
-      paths: {
-        // paths serve as alias
-        'npm:': 'node_modules/'
+  System.config({
+    paths: {
+      'npm:': 'https://unpkg.com/' // Le nouvel alias
+    },
+
+    map: {
+      app: 'dist',
+      '@angular/core': 'npm:@angular/core@6.0.3/bundles/core.umd.min.js',
+      '@angular/common': 'npm:@angular/common@6.0.3/bundles/common.umd.min.js',
+      '@angular/compiler': 'npm:@angular/compiler@6.0.3/bundles/compiler.umd.min.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser@6.0.3/bundles/platform-browser.umd.min.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@6.0.3/bundles/platform-browser-dynamic.umd.min.js',
+      '@angular/http': 'npm:@angular/http@6.0.3/bundles/http.umd.min.js',
+      '@angular/common/http': 'npm:@angular/common@6.0.3/bundles/common-http.umd.min.js',
+      '@angular/router': 'npm:@angular/router@6.0.3/bundles/router.umd.min.js',
+      '@angular/forms': 'npm:@angular/forms@6.0.3/bundles/forms.umd.min.js',
+      'rxjs': 'npm:rxjs@6.2.0',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api@0.6.0/bundles/in-memory-web-api.umd.js',
+      'tslib': 'npm:tslib/tslib.js'
+    },
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
       },
-      // map tells the System loader where to look for things
-      map: {
-        // our app is within the app folder
-        'app': 'dist/app',
-  
-        // angular bundles
-        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-        '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
-        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-  
-        // other libraries
-        'tslib': 'npm:tslib/tslib.js',
-        'rxjs': 'npm:rxjs',
-        'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      rxjs: {
+        main: 'index.js',
+        defaultExtension: 'js'
       },
-      // packages tells the System loader how to load when no filename and/or no extension
-      packages: {
-        app: {
-          defaultExtension: 'js',
-        },
-        rxjs: {
-          defaultExtension: 'js'
-        }
+      'rxjs/operators': {
+        main: 'index.js',
+        defaultExtension: 'js'
       }
-    });
+    }
+  });
   })(this);
